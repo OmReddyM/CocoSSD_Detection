f2a8cd758cce8aac9097535f0abfab659bf5cf34
OmReddyM <90816746+OmReddyM@users.noreply.github.com>
2022-03-05 15:35:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,38 @@
 img = "";
+Status = "";
 function preload() {
     img = loadImage('dog_cat.jpg');
 }
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+function modelLoaded() {
+    console.log("Model Loaded");
+    Status = true;
+    objectDetector.detect(img, gotResults);
+}
+function gotResults(error, results) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log(results);
+    }
 }
 function draw() {
     image(img, 0, 0, 640, 420);
+
     fill("#FF0000");
     text("Dog", 45, 75);
     noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 350);
+
+    fill("#FFF700")
+    text("Cat", 320, 120);
+    noFill();
+    stroke("#FFF700");
+    rect(300, 100, 270, 300);
 }
